d29baaeceadd1b7d4ce194dcc3dc247cd9b1c74a
t-jimbo <jbear.um@gmail.com>
2023-05-19 05:49:44 +0900

    feat: アラートを出す

--- web/src/features/mediaStream/Layout.tsx
+++ web/src/features/mediaStream/Layout.tsx
@@ -2,13 +2,25 @@ import Stack from "@mui/material/Stack";
 import { Outlet, useNavigate } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import { MediaStreamRecorderContext } from "./context.ts";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Button from "@mui/material/Button";
 import { Divider } from "@mui/material";
 
 export const Layout: React.FC = () => {
   const { isRecording } = useContext(MediaStreamRecorderContext);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const f = (e: BeforeUnloadEvent) => {
+      if (!isRecording) return;
+      // 録音中はページ遷移でアラートを出す
+      e.preventDefault();
+      e.returnValue = "";
+    };
+    window.addEventListener("beforeunload", f);
+
+    return () => window.removeEventListener("beforeunload", f);
+  }, [isRecording]);
 
   return (
     <Stack rowGap={1}>
